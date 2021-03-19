e822f3aab78157003582a393825a20e5f23b51ac
L <8lzkg8@gmail.com>
2021-03-19 00:47:52 -0600

    W9B Simple User Array

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
-var age = 18;
-var is_subscribed = true;
+// Creating 2 Arrays
+var userAges = [14, 18, 21];
+var subscriptionStatuses = [false, true, true];
 
-if (age < 18 && is_subscribed === false) {
-    console.log("The user is younger than 18 and is not subscribed");
-} else if (age >= 18 && is_subscribed === false) {
-    console.log("The user is 18 or older and is not subscribed");
-} else if (age < 18 && is_subscribed === true) {
-    console.log("The user is younger than 18 and is subscribed");
-} else if (age >= 18 && is_subscribed === true) {
-    console.log("The user is 18 or older and is subscribed");
-} else {
-    console.log("User is an Alien");
+// Looping userAges and subscriptionStatuses 3 times
+for (var counter = 0; counter < 3; counter++) {
+    // Setting the age from the userAges 
+    var age = userAges.pop();
+    // Setting the is_subscribed from the subscriptionStatuses
+    var is_subscribed = subscriptionStatuses.pop();
+
+    if (age < 18 && is_subscribed === false) {
+        console.log("The user is younger than 18 and is not subscribed");
+    } else if (age >= 18 && is_subscribed === false) {
+        console.log("The user is 18 or older and is not subscribed");
+    } else if (age < 18 && is_subscribed === true) {
+        console.log("The user is younger than 18 and is subscribed");
+    } else if (age >= 18 && is_subscribed === true) {
+        console.log("The user is 18 or older and is subscribed");
+    } else {
+        console.log("User is an Alien");
+    }    
 }
